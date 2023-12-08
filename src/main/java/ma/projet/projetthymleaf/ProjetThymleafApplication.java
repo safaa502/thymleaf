9f4a7de1fd71c2e4ffa3e17f558d12a9e0296198
safaa502 <elharrassafaa@gmail.com>
2023-12-08 12:37:42 +0100

    first commit

---
+++ src/main/java/ma/projet/projetthymleaf/ProjetThymleafApplication.java
@@ -0,0 +1,15 @@
+package ma.projet.projetthymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetThymleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetThymleafApplication.class, args);
+    }
+
+
+
+}
---
+++ src/test/java/ma/projet/projetthymleaf/ProjetThymleafApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.projet.projetthymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetThymleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
